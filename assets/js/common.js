7acfd2fd6ac48ec8b783fb39fe2da68a8fa0eb86
khalob <khalob1@yahoo.com>
2017-06-06 03:22:17 -0400

    Redesigned everything

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,33 @@
-  if ('addEventListener' in window) {
+if ('addEventListener' in window) {
 	window.addEventListener('load', function() {
 	  document.body.className = document.body.className.replace(/\bis-loading\b/, '');
 	});
-  }
-  
- function updateTitle(obj) {
+}
+
+function updateTitle(obj) {
 	var title = document.getElementById('pagesTitleDiv');
 	title.innerHTML = obj.children[0].getAttribute("title");
-  }
+}
+
+function fetchRepoLastUpdated() {
+	var repoLastUpdated = "Last updated: May 20, 2017";
+	$.ajax({
+		type: "GET",
+		url: "https://api.github.com/repos/khalob/khalob.github.io",
+		success: function(data){
+		  //Retrieve the last push time to the repo for update time
+		  repoLastUpdated = new Date(Date.parse(data.pushed_at)).toString();
+		  var wordsOfDate = repoLastUpdated.split(" ");
+		  var formmatedDate = wordsOfDate[1] + " " + wordsOfDate[2] + ", " + wordsOfDate[3]
+		  var curDateDiv = document.getElementById('curDate');
+		  curDateDiv.innerHTML = "Last updated: " + formmatedDate;
+		},
+		//On complete fail return err and set to sometime today
+		failure: function(errMsg) {
+			console.log(err);
+			repoLastUpdated = "Last updated: May 20, 2017";
+		}
+	});
+}
+  
+fetchRepoLastUpdated();
