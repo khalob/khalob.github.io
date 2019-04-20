0e6991712ec6a6aba345d9f301b051fc63a8ccaa
khalob <kcognata@emich.edu>
2019-04-20 18:21:45 -0400

    Update footer's 'lasted updated' date format.

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,26 +6,25 @@ if ('addEventListener' in window) {
 
 function updateTitle(obj) {
 	var title = document.getElementById('pagesTitleDiv');
-	title.innerHTML = obj.children[0].getAttribute("title");
+	title.innerHTML = obj.children[0].getAttribute('title');
 }
 
 function fetchRepoLastUpdated() {
-	var repoLastUpdated = "Last updated: May 20, 2017";
+	var repoLastUpdated = 'Last updated: May 20, 2017';
 	$.ajax({
-		type: "GET",
-		url: "https://api.github.com/repos/khalob/khalob.github.io",
+		type: 'GET',
+		url: 'https://api.github.com/repos/khalob/khalob.github.io',
 		success: function(data){
 		  //Retrieve the last push time to the repo for update time
-		  repoLastUpdated = new Date(Date.parse(data.pushed_at)).toString();
-		  var wordsOfDate = repoLastUpdated.split(" ");
-		  var formmatedDate = wordsOfDate[1] + " " + wordsOfDate[2] + ", " + wordsOfDate[3]
-		  var curDateDiv = document.getElementById('curDate');
-		  curDateDiv.innerHTML = "Last updated: " + formmatedDate;
+			var options = {year: 'numeric', month: 'long', day: 'numeric'};
+		  repoLastUpdated = new Date(Date.parse(data.pushed_at)).toLocaleDateString('en-US', options);
+			var curDateDiv = document.getElementById('curDate');
+		  curDateDiv.innerHTML = 'Last updated: ' + repoLastUpdated;
 		},
 		//On complete fail return err and set to sometime today
 		failure: function(errMsg) {
 			console.log(err);
-			repoLastUpdated = "Last updated: May 20, 2017";
+			repoLastUpdated = 'Last updated: May 20, 2017';
 		}
 	});
 }
@@ -35,5 +34,5 @@ fetchRepoLastUpdated();
 //events
 $('.page-link a').on('mouseover', function(){
   var $title = $('#titleDiv, #pagesTitleDiv');
-  $title.text(this.getAttribute("title"));
+  $title.text(this.getAttribute('title'));
 });
