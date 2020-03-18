f69c9e1ffea2e1a8fdb4909898e9cb6843b8a771
kcognata <kcognata@lyonscg.com>
2020-03-18 12:16:03 -0400

    Add Pokemon List react app files

---
+++ projects/webdev/react/pokemon-list/precache-manifest.20f040dab9f8b6a675e7e6d6db05e189.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ad3164a9839c86e2c4103172a52d72",
+    "url": "/projects/webdev/react/pokemon-list/index.html"
+  },
+  {
+    "revision": "40a42a96260dedfd7a62",
+    "url": "/projects/webdev/react/pokemon-list/static/css/main.2cd62c09.chunk.css"
+  },
+  {
+    "revision": "cab16ded9f8f0000d94e",
+    "url": "/projects/webdev/react/pokemon-list/static/js/2.ea7f6ccf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/projects/webdev/react/pokemon-list/static/js/2.ea7f6ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a42a96260dedfd7a62",
+    "url": "/projects/webdev/react/pokemon-list/static/js/main.69aa3578.chunk.js"
+  },
+  {
+    "revision": "8528bed21601e6286f33",
+    "url": "/projects/webdev/react/pokemon-list/static/js/runtime-main.9c1e36d9.js"
+  },
+  {
+    "revision": "f019b28580b874866cffb10198ce25a7",
+    "url": "/projects/webdev/react/pokemon-list/static/media/cardback.f019b285.png"
+  },
+  {
+    "revision": "9058a2301f70aef255a27591eb59d370",
+    "url": "/projects/webdev/react/pokemon-list/static/media/fire-bg.9058a230.png"
+  },
+  {
+    "revision": "74de8bff8cc247c7149310960639227d",
+    "url": "/projects/webdev/react/pokemon-list/static/media/flying-bg.74de8bff.jpg"
+  },
+  {
+    "revision": "20fe1e143ea1bb175a2035b1d180e398",
+    "url": "/projects/webdev/react/pokemon-list/static/media/grass-bg.20fe1e14.jpg"
+  },
+  {
+    "revision": "7c575b2898a794162369cb1db1ad2c36",
+    "url": "/projects/webdev/react/pokemon-list/static/media/normal-bg.7c575b28.png"
+  },
+  {
+    "revision": "b88d7dd8135e809d505fccd5d0573756",
+    "url": "/projects/webdev/react/pokemon-list/static/media/types-key.b88d7dd8.png"
+  },
+  {
+    "revision": "8123b9245fe07cc0ccb46f0ad925e521",
+    "url": "/projects/webdev/react/pokemon-list/static/media/types.8123b924.png"
+  },
+  {
+    "revision": "a50b9b3ebd50e1fbc2f0978b43039a1a",
+    "url": "/projects/webdev/react/pokemon-list/static/media/water-bg.a50b9b3e.jpg"
+  }
+]);
