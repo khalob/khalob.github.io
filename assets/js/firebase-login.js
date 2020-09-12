09b285f92541442d6f548e01bdf81af3ef636b36
khalob <kcognata@emich.edu>
2020-09-12 15:10:07 -0400

    WIP firebase authentication

---
+++ assets/js/firebase-login.js
@@ -0,0 +1,11 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+	signInFlow: 'popup',
+	signInOptions: [
+    // List of OAuth providers supported.
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID
+	]
+	// Other config options...
+});
