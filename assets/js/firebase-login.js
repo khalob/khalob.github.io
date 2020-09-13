0f9813beac563a1a6d02e3e818910f45607a69aa
khalob <kcognata@emich.edu>
2020-09-12 20:32:45 -0400

    WIP firebase authentication

--- assets/js/firebase-login.js
+++ assets/js/firebase-login.js
@@ -1,24 +1,17 @@
 
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    // User is signed out.
-	firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
-		// Existing and future Auth states are now persisted in the current
-		// session only. Closing the window would clear any existing state even
-		// if a user forgets to sign out.
+	if (user) {
+		// User is signed in.
+		var displayName = user.displayName;
+		var email = user.email;
+		var emailVerified = user.emailVerified;
+		var photoURL = user.photoURL;
+		var isAnonymous = user.isAnonymous;
+		var uid = user.uid;
+		var providerData = user.providerData;
 		// ...
-		// New sign-in will be persisted with session persistence.
-		
+	} else {
+		// User is signed out.
 		// Initialize the FirebaseUI so users can sign in
 		var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
@@ -30,14 +23,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 			]
 			// Other config options...
 		});
-		
-		return true;
-	}).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-	});
-  }
+	}
 });
   
   
